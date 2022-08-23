43c030f9866160cbc66f95f49d4c8f487422ad44
Júnior Veras <iramarjunhorveras@gmail.com>
2022-08-23 11:20:25 -0300

    Expressões Boleanas

---
+++ Java Estruturado - Academia dos Devs/Básico/src/expressoesboleanas.java
@@ -0,0 +1,27 @@
+import javax.swing.JOptionPane;
+
+public class expressoesboleanas {
+    public static void main(String[] args) {
+        
+        // int idade = 20;
+        // boolean resultado = idade >= 18;
+        // JOptionPane.showMessageDialog(null, resultado);
+        // idade = 11;
+        // resultado = idade >= 18;
+        // JOptionPane.showMessageDialog(null, resultado);
+        // idade = 18;
+        // resultado = idade == 18;
+        // JOptionPane.showMessageDialog(null, resultado);
+        // String nome = "Lucas";
+        // resultado = nome == "João";
+        // JOptionPane.showMessageDialog(null, resultado);
+
+        int episodiosAssistidos = 970;
+        int totalEpisodios = 970;
+
+        boolean terminou = episodiosAssistidos == totalEpisodios;
+
+        JOptionPane.showMessageDialog(null, terminou);
+
+    }
+}
