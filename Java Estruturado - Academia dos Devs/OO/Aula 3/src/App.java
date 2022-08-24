41aeac1da4bf9c285a0d8d604c5dcd46e4bec946
Júnior Veras <iramarjunhorveras@gmail.com>
2022-08-24 18:13:39 -0300

    Aula 3 - Pacotes

---
+++ Java Estruturado - Academia dos Devs/OO/Aula 3/src/App.java
@@ -0,0 +1,22 @@
+import br.com.devflix.gui.*;
+import br.com.devflix.gui.botoes.*;
+import br.com.devflix.videos.*;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        Filme filme = new Filme();
+        Novela novela = new Novela();
+        Documentario documentario = new Documentario();
+        Anime anime = new Anime();
+        Serie serie = new Serie();
+
+        Janela janelinha = new Janela();
+        MiniaturaVideo miniatura = new MiniaturaVideo();
+        BotaoPlay play = new BotaoPlay();
+        BotaoPause pause = new BotaoPause();
+        BotaoVoltar voltar = new BotaoVoltar();
+
+
+    }
+}
