a76388f4c0553c54ff90212414c8831b845292d9
Júnior Veras <iramarjunhorveras@gmail.com>
2022-08-24 07:41:45 -0300

    Condicoes

---
+++ Java Estruturado - Academia dos Devs/Básico/src/Condicoes.java
@@ -0,0 +1,24 @@
+public class Condicoes {
+    public static void main(String[] args) {
+
+        int idade = 17;
+        if (idade >= 18)
+        {
+         System.out.println("Sua idade é maior ou igual a 18");   
+        }
+        else
+        {
+            System.out.println("Sua idade é menor que 18");
+        }
+
+        boolean estaChovendo = false;
+
+        if (estaChovendo == false) {
+            System.out.println("Não está chovendo");
+        }
+        else
+        {
+            System.out.println("Está chovendo");
+        }
+    }
+}
