9e4ed89b196b9176d42389ed28fe7389aa83b4c8
Júnior Veras <iramarjunhorveras@gmail.com>
2022-12-01 19:09:20 -0300

    UPDATE

--- TrabalhoFinal/TrabalhoFinal/src/App.java
+++ TrabalhoFinal/TrabalhoFinal/src/App.java
@@ -1,10 +1,8 @@
-import java.util.Scanner;
-
 import Entities.Agenda;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        Scanner scan = new Scanner(System.in);
+
         ArrayList<Agenda> agenda = new ArrayList<Agenda>();
 
         System.out.println("Digite o Nome da Agenda");
