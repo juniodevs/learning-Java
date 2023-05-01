5af35891c68963754128206cc365d3f4bf03eb87
Júnior Veras <iramarjunhorveras@gmail.com>
2023-05-01 11:59:27 -0300

    UPDATE

---
+++ Testes/ATIVIDADES/src/q1.java
@@ -0,0 +1,7 @@
+public class q1 {
+    public static void main(String[] args) throws Exception {
+    for(int i = 0; i <= 99; i++){
+        System.out.println(i);
+    }
+    }
+}
---
+++ Testes/ATIVIDADES/src/q2.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class q2 {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        String numeros = sc.nextLine(), nstr = "";
+        char ch;
+        for(int i = 0; i<numeros.length(); i++)
+        {
+            ch = numeros.charAt(i);
+            if(ch < ch+2){
+            nstr = ch + nstr;
+            }
+        }
+        System.out.println(nstr);
+    }
+}
